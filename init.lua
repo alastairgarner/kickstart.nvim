4a42f5e866e521efbd1715f175169f27efbf8725
alastair <garneralastair@gmail.com>
2024-12-23 11:16:45 +0000

    Remove comment

--- init.lua
+++ init.lua
@@ -841,15 +841,6 @@ require('lazy').setup({
     end,
   },
 
-  -- {
-  --   'catppuccin/nvim',
-  --   name = 'catppuccin',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'catppuccin-mocha'
-  --   end,
-  -- },
-
   {
     'sainnhe/sonokai',
     priority = 1000,
